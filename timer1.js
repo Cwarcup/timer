4506281359c338f4f254b6565a1d2fae55c183ca
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-07-05 13:56:18 -0700

    edge cases for timer1

--- timer1.js
+++ timer1.js
@@ -1,7 +1,11 @@
 // get args from command line
 const argsArr = process.argv.slice(2);
 
+
 for (let i = 0; i < argsArr.length; i++) {
+  if (argsArr[i] < 0 || typeof argsArr[i] !== 'number') {
+    return;
+  }
   setTimeout(() => {
     process.stdout.write('\x07');
   }, argsArr[i] * 1000);
